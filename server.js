952f3a96c781fd2734b6a6372ea41befc9f1fe9f
Pjae215 <lala2462002@hotmail.com>
2019-10-10 18:00:00 -0400

    easier with handlebars

--- server.js
+++ server.js
@@ -1,134 +1,97 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const newyorker = require("./news");
+const models = require("./models");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+const app = express();
 
-// Require all models
-var db = require("./models");
-
-var PORT = process.env.PORT || 3000;
-
-// Initialize Express
-var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
+// Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-// Connect to the Mongo DB
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-mongoose.connect(MONGODB_URI);
+// Setup database
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/newyorker';
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+const Article = models.Article;
+const Comment = models.Comment;
+
 
 // Routes
-
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.bioethics.net/news").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("h5").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
+app.get('/', (req, res) => {
+    Article.find({}).sort({ date: -1 })
+        .then((articles) => {
+            res.render('index', {
+                articles: articles
+            })
         })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
+});
+
+
+app.get('/articles/:id', (req, res) => {
+    const id = req.params.id;
+    Article.findById(id).populate("comments").exec()
+        .then((article) => {
+            res.render("more", article);
         });
-    });
-
-    // Send a message to the client
-    res.send("Scrape Complete!");
-  });
 });
 
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
-    // Find all results from the scrapedData collection in the db
-    db.Article.find()
-      // Throw any errors to the console
-      .then(function(dbPopulate) {
-        // If any Libraries are found, send them to the client with any associated Books
-        res.json(dbPopulate);
-      })
-      .catch(function(err) {
-        // If an error occurs, send it back to the client
-        res.json(err);
-      });
+
+app.post('/articles/:id/comments', (req, res) => {
+    const articleId = req.params.id;
+    const commentText = req.body.text;
+
+    // Create a comment
+    Comment.create({ text: commentText, date: new Date() })
+        .then((comment) => {
+            
+
+            return Article.findByIdAndUpdate(articleId, { $push: { comments: comment._id } }, { new: true })
+        })
+        .then((article) => {
+            res.redirect("/articles/" + articleId);
+        })
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // Finish the route so it finds one article using the req.params.id,
-  // and run the populate method with "note",
-  // then responds with the article with the note included
-  db.Article.findById(req.params.id)
-  .populate("note")
-  .then(function(dbPopulate) {
-    // If any Libraries are found, send them to the client with any associated Books
-    res.json(dbPopulate);
-  })
-  .catch(function(err) {
-    // If an error occurs, send it back to the client
-    res.json(err);
-  });
+app.delete('/articles/:articleId/comments/:commentId', (req, res) => {
+    const articleId = req.params.articleId;
+    const commentId = req.params.commentId;
+
+    Article.findByIdAndUpdate(articleId, { $pull: { comments: commentId } })
+        .then(() => {
+            Comment.findByIdAndDelete(commentId)
+                .then(() => res.sendStatus(200));
+        })
+
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
-  db.Note.create(req.body)
-    .then(function(dbPopulate) {
-      
-      return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbPopulate._id } }, { new: true });
-    })
-    .then(function(dbPopulate) {
-      // If the Library was updated successfully, send it back to the client
-      res.json(dbPopulate);
-    })
-    .catch(function(err) {
-      // If an error occurs, send it back to the client
-      res.json(err);
+
+app.post("/api/scrape", (req, res) => {
+    newyorker.scrape(function (newsArticles) {
+
+        // Add all news articles to MongoDB
+        Article.insertMany(newsArticles, { ordered: false }, function (err, articles) {
+            if (!err) {
+                // All articles were inserted successfully
+                console.log("Articles inserted: " + articles.length);
+                res.json({ count: articles.length });
+            }
+            else if (err.result.ok) {
+                // Some articles may have been duplicates
+                console.log("Articles inserted: " + err.result.nInserted);
+                res.json({ count: err.result.nInserted });
+            }
+            else {
+                console.log(response);
+                res.staus(500).json(response.result);
+            }
+        })
     });
 });
 
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server listening at port " + PORT));
